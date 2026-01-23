f407321713bafb04c41d5bccb8f0c1311897b31c
unknown <chunyu0325@foxmail.com>
2026-01-23 23:09:36 +0800

    add king movies windy gridworld

---
+++ section_6_4_king_moves_windy_gridworld.py
@@ -0,0 +1,19 @@
+from section_6_4_windy_gridworld import *
+
+# exercise 6.4
+V = np.zeros((nrow, ncol, 8))
+action_space = action_space + [(i, j) for i in [-1, 1] for j in [-1, 1]]
+run(V, action_space)
+
+
+# exercise 6.5
+def row_stochastic():
+    return np.random.choice([-1, 0, 1])
+
+
+run(V, action_space, row_stochastic=row_stochastic)
+
+# exercise 6.4
+V = np.zeros((nrow, ncol, 9))
+action_space = action_space + [(0, 0)]
+run(V, action_space)
--- section_6_4_windy_gridworld.py
+++ section_6_4_windy_gridworld.py
@@ -2,29 +2,30 @@ from utils.gridworld_util import *
 from utils.tabular_util import *
 
 nrow, ncol = 7, 10
-V = np.zeros((nrow, ncol, 4))
-
 wind = [0, 0, 0, -1, -1, -1, -2, -2, -1, 0]
 
+def default_row_stochastic():
+    return 0
 
-def sarsa(n, alpha, epsilon, *, V, action_space, nrow, ncol, T):
+
+def sarsa(n, alpha, epsilon, *, V, action_space, nrow, ncol, T, row_stochastic):
     episode_time = []
     t = 0
     for i in range(n):
         s = (3, 0)
         a = greedy(s, epsilon=epsilon, V=V, action_space=action_space)
         while s != (3, 7):
-            t += 1
-            episode_time.append([i, t])
-            if len(episode_time) ==  T:
-                return episode_time
-            
             r = -1
-            row, col = s[0] + action_space[a][0] + wind[s[1]], s[1] + action_space[a][1]
+            row, col = s[0] + action_space[a][0] + wind[s[1]] + row_stochastic(), s[1] + action_space[a][1]
             s_next = (int(np.clip(row, 0, nrow-1)), int(np.clip(col, 0, ncol-1)))
             a_next = greedy(s_next, epsilon=epsilon, V=V, action_space=action_space)
 
             V[*s][a] += alpha * (r + V[*s_next][a_next] - V[*s][a])
+
+            t += 1
+            episode_time.append([i, t])
+            if len(episode_time) ==  T:
+                return episode_time
 
             s, a = s_next, a_next
         
@@ -32,8 +33,14 @@ def sarsa(n, alpha, epsilon, *, V, action_space, nrow, ncol, T):
     return episode_time
 
 
-episode_time = sarsa(200, alpha=.5, epsilon=.1, V=V, action_space=action_space, nrow=nrow, ncol=ncol, T=8000)
+def run(V, action_space, row_stochastic=default_row_stochastic):
+    episode_time = sarsa(200, alpha=.5, epsilon=.1, V=V, action_space=action_space, nrow=nrow, ncol=ncol, T=8000, row_stochastic=row_stochastic)
 
-# print(episode_time)
-plt.plot(np.array(episode_time)[:,1], np.array(episode_time)[:,0])
-plt.show()
+    # print(episode_time)
+    plt.plot(np.array(episode_time)[:,1], np.array(episode_time)[:,0])
+    plt.show()
+
+
+if __name__ == '__main__':
+    V = np.zeros((nrow, ncol, 4))
+    run(V, action_space)
